68841a138a971ee2edb73ace03a5749c97fab9f6
Hilda1060 <caminoalexitoconhildapereira@gmail.com>
2026-08-16 14:54:27 -0500

    Delete cn.ts

--- cn.ts
+++
@@ -1,6 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-
-/** Une clases condicionales. Mantén las utilidades de Tailwind ordenadas y legibles. */
-export function cn(...inputs: ClassValue[]): string {
-  return clsx(inputs);
-}
